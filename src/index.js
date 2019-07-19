742b4a435d6a660a99173018588e602b94977e1a
Trey Arnold <arnold.trey80@gmail.com>
2019-07-19 17:29:12 -0500

    Inital file structure and schema

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  },
+  {
+    id: "link-1",
+    url: "www.espn.com",
+    description: " Sport the sports"
+  },
+]
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs : "./src/schema.graphql",
+  resolvers,
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
