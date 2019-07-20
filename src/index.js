e12194c1ac43e45e951d1f775c168a7be7e44b00
Trey Arnold <arnold.trey80@gmail.com>
2019-07-19 20:04:08 -0500

    created prisma client and data model

--- src/index.js
+++ src/index.js
@@ -6,22 +6,25 @@ let links = [
     url: "www.howtographql.com",
     description: "Fullstack tutorial for GraphQL"
   },
-  {
-    id: "link-1",
-    url: "www.espn.com",
-    description: " Sport the sports"
-  },
-]
+];
+
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link);
+      return link;
+    }
   }
 };
 
